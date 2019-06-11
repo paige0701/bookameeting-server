b40a1495b5e7901f152bae6e1b2dfb2298b0fdbc
paige0701 <paigechoi0701@gmail.com>
2019-06-11 16:19:07 +0900

    fix project structure

---
+++ flaskr/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+
+class MeetingRoom(db.Model):
+    __tablename__ = 'meeting_room'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
